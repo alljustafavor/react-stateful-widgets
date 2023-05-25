1c3fee023a1c7734ede8cbacea437a42f34fde6b
Joseph Noblett <noblettjoe@gmail.com>
2023-05-25 12:43:23 -0500

    updated the map on programmers then on squares I init the useStates and updated that map as well

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,7 +14,7 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
@@ -24,6 +24,8 @@ export default function Squares() {
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
+  const [squares, setSquareIds] = useState(listOfSquareIds);
+  const [activeSquare, setActiveSquare] = useState(null);
 
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
@@ -48,7 +50,7 @@ export default function Squares() {
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
