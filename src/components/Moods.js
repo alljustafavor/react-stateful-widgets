b5f20049a5c0eca8ab442875be12c0860fc9a296
Joseph Noblett <noblettjoe@gmail.com>
2023-05-25 11:44:59 -0500

    all tasks completed

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -35,16 +35,16 @@ const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
 
 export default function Moods() {
-  const [mood, setMood] = useState(happyMood);
+  const [mood, setMood] = useState(initialMood);
 
   const makeHappy = () => {
-    /* STEP 4 */
+    setMood(happyMood);
   };
   const makeSad = () => {
-    /* STEP 5 */
+    setMood(sadMood);
   };
   const reset = () => {
-    /* STEP 6 */
+    setMood(initialMood)
   };
 
   const style = {
